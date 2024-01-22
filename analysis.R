4eb631dfabed9912ba65ed04383c4cb760cf3c32
Samir Ouijdani <149670666+SamirOuij@users.noreply.github.com>
2024-01-22 15:30:51 -0800

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -1,30 +1,13 @@
 # Overview ----------------------------------------------------------------
 
-# Assignment 2: U.S. COVID Trends
 # For each question/prompt, write the necessary code to calculate the answer.
-# For grading, it's important that you store your answers in the variable names
-# listed with each question in `backtics`. Please make sure to store the
-# appropriate variable type (e.g., a string, a vector, a dataframe, etc.)
-# For each prompt marked `Reflection`, please write a response
-# in your `README.md` file.
 
 
-
-# Loading data ------------------------------------------------------------
-
-# You'll load data at the national, state, and county level. As you move through
-# the assignment, you'll need to consider the appropriate data to answer
-# each question (though feel free to ask if it's unclear!)
-
-
-
-# Load the tidyverse package
 
 library("tidyverse")
 
 
 # Load the *national level* data into a variable. `national`
-# (hint: you'll need to get the "raw" URL from the NYT GitHub page)
 
 national<-read.csv("C:\\Users\\samir\\Downloads\\us.csv")
 
@@ -76,17 +59,6 @@ num_features_counties <- ncol(counties)
 
 
 # Exploratory analysis ----------------------------------------------------
-
-# For this section, you should explore the dataset by answering the following
-# questions. HINT: Remember that in class, we talked about how you can answer 
-# most data analytics questions by selecting specific columns and rows. 
-# For this assignment, you are welcome to use either base R dataframe indexing or
-# use functions from the DPLYR package (e.g., using `pull()`). Regardless, you 
-# must return the specific column being asked about. For example, if you are 
-# asked the *county* with the highest number of deaths, your answer should
-# be a single value (the name of the county: *not* an entire row of data).
-# (again, make sure to read the documentation to understand the meaning of
-# each row -- it isn't immediately apparent!)
 
 # How many total cases have there been in the U.S. by the most recent date
 # in the dataset? `total_us_cases`
@@ -156,13 +128,7 @@ county_highest_cases #Los Angeles County
 num_highest_cases_county <- max(counties$cases)
 num_highest_cases_county #1495718
 
-# Because there are multiple counties with the same name across states, it
-# will be helpful to have a column that stores the county and state together
-# (in the form "COUNTY, STATE").
-# Add a new column to your `counties` data frame called `location`
-# that stores the county and state (separated by a comma and space).
-# You can do this by mutating a new column, or using the `unite()` function
-# (just make sure to keep the original columns as well)
+
 counties <- unite(counties, "location", county:state, sep = ", " , na.rm = TRUE, remove = FALSE)
 counties
 
@@ -173,25 +139,10 @@ location_most_deaths_num <- max(counties$deaths)
 location_most_deaths<- filter(counties, deaths == location_most_deaths_num)
 location_most_deaths #New York City, NY
 
-# Reflection: Is the location with the highest number of cases the location with
-# the most deaths? If not, why do you believe that may be the case?
-
-#The location with the most cases is not the one with the most deaths, this could
-#be because access to healthcare is poorer in NY than LA
-
-
-# At this point, you (hopefully) have realized that the `cases` column *is not*
-# the number of _new_ cases in a day (if not, you may need to revisit your work)
-# Add (mutate) a new column on your `national` data frame called `new_cases`
-# that has the nubmer of *new* cases each day (hint: look for the `lag`
-# function).
 
 national$new_cases <- national$cases - lag(national$cases, 1)
 
 
-# Similarly, the `deaths` columns *is not* the number of new deaths per day.
-# Add (mutate) a new column on your `national` data frame called `new_deaths`
-# that has the nubmer of *new* deaths each day
 
 national$new_deaths <- national$deaths - lag(national$deaths, 1)
 
@@ -214,14 +165,7 @@ most_deaths #99627
 
 # Grouped analysis --------------------------------------------------------
 
-# An incredible power of R is to perform the same computation *simultaneously*
-# across groups of rows. The following questions rely on that capability.
 
-# What is the county with the *current* (e.g., on the most recent date)
-# highest number of cases in each state? Your answer, stored in
-# `highest_in_each_state`, should be a *vector* of
-# `location` names (the column with COUNTY, STATE).
-# Hint: be careful about the order of filtering your data!
 
 highest_in_each_state <- counties %>%
   group_by(state) %>%
@@ -230,10 +174,7 @@ highest_in_each_state <- counties %>%
   pull(location) #Jefferson, Alabama
 
 
-# What is the county with the *current* (e.g., on the most recent date)
-# lowest number of deaths in each state? Your answer, stored in
-# `lowest_in_each_state`, should be a *vector* of
-# `location` names (the column with COUNTY, STATE).
+
 
     lowest_in_each_state <- counties %>%
     group_by(state)%>%
@@ -242,22 +183,6 @@ highest_in_each_state <- counties %>%
     pull(location)  
     lowest_in_each_state
     
-# Reflection: Why are there so many observations (counties) in the variable
-# `lowest_in_each_state` (i.e., wouldn't you expect the number to be ~50)?
-    
-    #Because there are overseas territories as well as some counties
-    #that are tied for the lowest in their respective territories
-
-
-# The following is a check on our understanding of the data.
-# Presumably, if we add up all of the cases on each day in the
-# `states` or `counties` dataset, they should add up to the number at the
-# `national` level. So, let's check.
-
-# First, let's create `state_by_day` by adding up the cases on each day in the
-# `states` dataframe. For clarity, let's call the column with the total cases
-# `state_total`
-# This will be a dataframe with the columns `date` and `state_total`.
     
     state_by_day <- states %>%
     group_by(dates)%>%
@@ -266,23 +191,15 @@ highest_in_each_state <- counties %>%
     summarize(state_total - sum(cases))
 
 
-# Next, let's create `county_by_day` by adding up the cases on each day in the
-# `counties` dataframe. For clarity, let's call the column with the total cases
-# `county_total`
-# This will also be a dataframe, with the columns `date` and `county_total`.
-     
       county_by_day <- counties %>%
       group_by(dates)%>%
       county_total <- sum(cases)%>%
       summarize(county_total - sum(counties$cases))
 
-# Now, there are a few ways to check if they are always equal. To start,
-# let's *join* those two dataframes into one called `totals_by_day`
+
 
     totals_by_day <- merge(county_total, state_total)
 
-# Next, let's create a variable `all_totals` by joining `totals_by_day`
-# to the `national` dataframe
 
 all_totals <- national  %>%
   mutate(totals_by_day)
@@ -307,9 +224,7 @@ all_totals <- national  %>%
 # and add up all of the cases to the state level on each day (e.g.,
 # aggregating to the state level). Store this dataframe with three columns
 # (state, date, county_totals) in the variable `sum_county_to_state`.
-# (To avoid DPLYR automatically grouping your results,
-# specify `.groups = "drop"` in your `summarize()` statement. This is a bit of
-# an odd behavior....)
+
 
 sum_county_to_state <- counties %>%
   group_by(date, state) %>%
@@ -320,50 +235,16 @@ sum_county_to_state <- counties %>%
  
 joined_states <- left_join(sum_county_to_state, states)
 
-# To find out where (and when) there is a discrepancy in the number of cases,
-# create the variable `has_discrepancy`, which has *only* the observations
-# where the sum of the county cases in each state and the state values are
-# different. This will be a *dataframe*.
 
 has_discrepancy <- joined_states %>%
   filter(county_totals != cases)
 
-# Next, lets find the *state* where there is the *highest absolute difference*
-# between the sum of the county cases and the reported state cases.
-# `state_highest_difference`.
-# (hint: you may want to create a new column in `has_discrepancy` to do this.)
+
 
 states_highest_difference <- has_discrepancy %>%
   mutate(state_difference = abs(county_totals - cases)) %>%
   filter(state_difference == max(state_difference))%>%
   pull(state)
 states_highest_difference
-
-# Independent exploration -------------------------------------------------
-
-# Ask your own 3 questions: in the section below, pose 3 questions,
-# then use the appropriate code to answer them.
-
-#How long of a time period are we working with
-first_day <- min(national$date) #2020-01-21
-
-
-last_day <- max(national$date) #2021-11-02
-
-difftime(first_day, last_day) #650 days
-
-#What is the average number of cases per day in counties
-avg_cases <- mean(counties$cases)
-avg_cases #6418.091
-
-#what state had the lowest death ratio 
-state_lowest_ratio_number <- min(states$death_ratio)
-state_lowest_ratio <- filter(states, death_ratio == state_lowest_ratio_number)
-state_lowest_ratio #it was a tie for quite a few states at 0
-
-
-# Reflection: What surprised you the most throughout your analysis?
-# One of the most surprising things for me was how accurate the death counts were 
-#between states, counties and nationally, I was expecting a larger discrepancy
 
 
